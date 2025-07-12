4db78336b6753a2682bfaedd08c5337ece864dff
XuanYi-723 <wu0966014739@gmail.com>
2025-07-12 17:00:44 +0800

    Update test.py
    add comment

--- src/test/test.py
+++ src/test/test.py
@@ -1,8 +1,8 @@
-import time
+import time            #時間處理模組
 
-import streamlit as st
+import streamlit as st #互動式網頁應用(資料科學)
 
-import numpy as np
-import pandas as pd
+import numpy as np     #處理數值運算和矩陣
+import pandas as pd    #處理表格型資料
 
-st.title('我的第一個應用程式')
+st.title('我的第一個應用程式') #頁面標題
