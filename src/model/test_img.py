41f7de66db6f714a8d7f3aaf30fb5a431823af92
peter95514 <peter95514@gmail.com>
2025-07-17 16:04:08 +0800

    test_img ver1.1

--- src/model/test_img.py
+++ src/model/test_img.py
@@ -1,9 +1,16 @@
 from ultralytics import YOLO
 import os
+import cv2
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+img_name = os.path.join(root, "data/all/1.3.6.1.4.1.14519.5.2.1.6655.2359.112545118683956537398940148278.png")
 
 model = YOLO(os.path.join(root, "runs/detect/lung_tumor_model_ver1.05/weights/best.pt"))
 
-results = model(os.path.join(root, "data/all/1.3.6.1.4.1.14519.5.2.1.6655.2359.112545118683956537398940148278.png"))
-results[0].plot()
+results = model(img_name)
+result = results[0].plot()
+
+output_path = os.path.join(root, "data/output_pre_img")
+os.makedirs(output_path, exist_ok=True)
+
+cv2.imwrite(os.path.join(output_path, os.path.basename(img_name)), result)
